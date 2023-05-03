a25503667ec651d7f165d075bf31972f34f26c25
christosmil <48249186+christosmil@users.noreply.github.com>
2023-05-03 12:42:45 +0300

    pep8, pylint acronyms.py

--- acronyms.py
+++ acronyms.py
@@ -1,77 +1,80 @@
+"""
+A module to read abbreviations from a .txt file, remove duplicates,
+sort them, format them for 'acronyms' package for latex, and store them
+in a .tex file.
+"""
+
 import sys
 
-OPEN_BRACKET = "{"
-CLOSE_BRACKET = "}"
+# Set filenames.
+READ_FILE = 'abbreviations.txt'
+STORE_FILE = f"acronyms.tex"
 
-def checkDuplicates(lines): # 'lines' are a list => mutable object
-	'''
-	Check if there are duplicates; if yes, remove them.
-	'''
-	if len(lines) == len(set(lines)):
-		print('[NO DUPLICATES]')
-	else:
-		duplicates_list = []
-		print('[DUPLICATES FOUND]')
-		for index in range(len(lines) - 1):
-			if lines[index] == lines[index + 1]:
-				duplicates_list.append(lines[index])
-				print(f'[DUPLICATE] {lines[index]}')
-		for i in duplicates_list:
-			print(f'[REMOVING DUPLICATE] {i}')
-			lines.remove(i)
 
-def formatAconymsList(lines):
-	'''
-	Create the string that contains all the acronyms formatted for the
-	output file. Two reasons to do this:
-	1. Write in one passage to the file, in the next block of commands
-	2. We need to find the longest acronym label to use it for alignment
-	'''
-	acronyms = ""
-	max_acronym_label = ""
-	for i in lines:
-		new_line = i.split(" ", 1)
-		if len(max_acronym_label) < len(new_line[0]):
-			max_acronym_label = new_line[0]
-		acronyms += f"\\acro{OPEN_BRACKET}{new_line[0]}{CLOSE_BRACKET}{OPEN_BRACKET}{new_line[1]}{CLOSE_BRACKET}\n"
-	
-	return acronyms, max_acronym_label
+def read_abbreviations(filename):
+    """Reads the abbreviations from the text file."""
+    try:
+        with open(filename, encoding='utf-8') as f:
+            abbreviations = f.read().split('\n')
+    except FileNotFoundError:
+        sys.exit(f"File \" {filename} \" does not exist.")
+    else:
+        abbreviations.pop()
 
-def writeToFile(output_file, acronyms, max_acronym_label):
-	'''
-	Write to file. The format is:
-	\begin{acronym}[longest-acronym-label]
-	\acro{acro-label-1}{Acronym-1}
-	\acro{acro-label-2}{Acronym-2}
-	...
-	\acro{acro-label-N}{Acronym-N}
-	\end{acronym}
-	'''
-	with open(output_file, "w+") as f:
-		f.write(f"\\begin{OPEN_BRACKET}acronym{CLOSE_BRACKET}[{max_acronym_label}]\n")
-		f.write(f"{acronyms}")
-		f.write(f"\\end{OPEN_BRACKET}acronym{CLOSE_BRACKET}\n")
-	print(f'[ACRONYMS FILE CREATED] File: {output_file}')
+        return abbreviations
+
+
+def remove_duplicates(abbreviations):
+    """Finds and removes any duplicate abbreviations."""
+    if len(abbreviations) == len(set(abbreviations)):
+        print('No duplicates!')
+    else:
+        print('Duplicates!')
+        copy_abbrs = abbreviations[:]
+        abbreviations = []
+        for abbreviation in copy_abbrs:
+            if abbreviation in abbreviations:
+                print(f"Removing '{abbreviation}'...")
+            else:
+                abbreviations.append(abbreviation)
+    return abbreviations
+
+
+def format_acronyms(abbreviations):
+    '''
+    Creates the string that contains all the acronyms formatted for the
+    output file. Two reasons to do this:
+    1. Write in one passage to the file, in the next block of commands.
+    2. Find the longest acronym label, to use it foralignment.
+    '''
+    acronyms = ""
+    max_acronym_label = ""
+    for abbreviation in abbreviations:
+        new_line = abbreviation.split(" ", 1)
+        if len(max_acronym_label) < len(new_line[0]):
+            max_acronym_label = new_line[0]
+        acronyms += f"\\acro{{{new_line[0]}}}{{{new_line[1]}}}\n"
+
+    return acronyms, max_acronym_label
+
+
+def store_abbreviations(filename, acronyms, max_acronym_label):
+    """Stores abbreviations to the file."""
+    with open(filename, "w+", encoding='utf-8') as f:
+        f.write(f"\\begin{{acronym}}[{max_acronym_label}]\n")
+        f.write(f"{acronyms}")
+        f.write("\\end{{acronym}}\n")
+    print(f'[ACRONYMS FILE CREATED] File: {filename}')
+
+
+def main():
+    """Runs the program."""
+    abbrs = read_abbreviations(READ_FILE)
+    abbrs_deduplicated = remove_duplicates(abbrs)
+    abbrs_deduplicated.sort()
+    acronyms, max_acronym_label = format_acronyms(abbrs)
+    store_abbreviations(STORE_FILE, acronyms, max_acronym_label)
+
 
 if __name__ == "__main__":
-	'''
-	Define the input file with the acronyms and the output file with the
-	acronyms formatted for latex.
-	'''
-	INPUT_FILE = 'acronyms.txt'
-	OUTPUT_FILE = f"{INPUT_FILE[:-4]}.tex"
-	
-	'''
-	Check if input file exists
-	'''
-	try:
-		data = open(INPUT_FILE, 'r')
-		lines = data.read().split('\n')
-		data.close()
-	except FileNotFoundError:
-		sys.exit(f"[FILE NOT FOUND] File \"{INPUT_FILE}\" does not exist.")
-
-	lines.sort()
-	checkDuplicates(lines)
-	acronyms, max_acronym_label = formatAconymsList(lines)
-	writeToFile(OUTPUT_FILE, acronyms, max_acronym_label)
+    main()
